0fdd9a10f057a0bc52aaac37be205459d1d699dd
Szymon Boćkowski <sbockowx@gmail.com>
2025-11-02 20:24:49 +0100

    First code in main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from db.connection import init_db
-from services.budget import created_budget
+from services.budget import create_budget
 
 def main():
     init_db()
-    print("Budżet rachu-ciach")
+    print("Budżet rachu-ciach!")
+    budget_name = input("Podaj nazwę budżetu: ")
+    budget_id = create_budget(budget_name)
+    print(f"Utworzono budżet id={budget_id},name={budget_name}")
+
+if __name__ == "__main__":
+    main()
--- services/budget.py
+++ services/budget.py
@@ -1,6 +1,6 @@
 from db.connection import get_conn
 
-def created_budget(name: str):
+def create_budget(name: str):
     with get_conn() as conn:
         cur = conn.cursor()
         cur.execute("INSERT INTO budget (name) VALUES (?)",(name,))
